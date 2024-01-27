41c611f672bba79ab4fe852afbc26ec3895f9db4
brunobrunoo114 <truongtritin.bee@gmail.com>
2024-01-27 22:52:12 +0700

    feat: update blog post

--- next.config.js
+++ next.config.js
@@ -8,6 +8,11 @@ const nextConfig = {
       },
     ],
   },
+  env: {
+    NOTION_DATABASE_ID: "51b1c5db34e24dbb8e38ff01139a6e07",
+    NOTION_INTERNAL_INTEGRATION_TOKEN:
+      "secret_N0XxyyOsDhmmLImH41H26A8mVq8AAMeh2qSBYIt3L7Z",
+  },
 };
 
 module.exports = nextConfig;
