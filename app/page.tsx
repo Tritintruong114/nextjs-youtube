e627c9af0052047c18aa9fedbfe639ec4f68bea1
brunobrunoo114 <truongtritin.bee@gmail.com>
2024-02-04 17:54:55 +0700

    feat: update

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,47 @@
+/* eslint-disable react/no-unescaped-entities */
 "use client";
-import HomepageCta from "@/pages/homepage/cta";
-import HomepageExpertise from "@/pages/homepage/expertise";
-import HompageHero from "@/pages/homepage/hero";
-import HomepageProject from "@/pages/homepage/project";
-import HomepageValue from "@/pages/homepage/value";
+
+import ArrowRight from "@/public/icons/arrow-right";
+
 export default function Home() {
   return (
-    <main className="rounded-3xl h-fit overflow-y-scroll  no-scrollbar">
-      <HompageHero />
-      <HomepageCta />
-      <HomepageProject />
-      <HomepageExpertise />
-      <HomepageValue />
+    <main className=" h-screen flex  w-screen justify-center items-center overflow-y-scroll no-scrollbar">
+      <ul className="w-full">
+        <li className="flex w-full items-center relative group h-fit overflow-hidden">
+          <div className="flex group w-full">
+            <span className="font-bebas w-full text-[6vw] leading-none relative pt-2 text-white bg-black duration-300 ease-in-out transition-transform group-hover:-translate-y-[100%]">
+              "Creative Development"
+            </span>
+            <span className="font-bebas w-full text-[6vw] leading-none absolute pt-2 text-black bg-white duration-300 ease-in-out transition-transform translate-y-[100%] group-hover:-translate-y-[0%]">
+              "Creative Development"
+            </span>
+          </div>
+          <div className="z-10 absolute flex group w-fit overflow-hidden right-0">
+            <ArrowRight className="group-hover:translate-x-full w-[30px] sm:w-[60px] md:w-[90px] bg-transparent duration-300 fill-white" />
+            <ArrowRight className="absolute bg-transparent left-0 -translate-x-full group-hover:translate-x-0 duration-300 fill-black" />
+          </div>
+        </li>
+        <li className="flex w-full items-center relative group h-fit overflow-hidden">
+          <div className="flex group w-full">
+            <span className="font-bebas w-full text-[6vw] leading-none relative pt-2 text-white/60 bg-black duration-300">
+              "Creative Development"
+            </span>
+          </div>
+          {/* <div className="group-hover:opacity-100 opacity-0 bg-black px-1 text-white absolute w-full duration-150 -rotate-3">
+            <Marquee speed={15} autoFill>
+              <span className="px-1">-coming soon</span>
+            </Marquee>
+          </div> */}
+          <button className="bg-white flex h-fit justify-center relative items-center group/button overflow-hidden text-black px-base py-1 rounded right-3 duration-300 group-hover:bottom-0 -bottom-24">
+            <span className="duration-150 leading-none group-hover/button:top-full relative">
+              Subscribe
+            </span>
+            <span className="absolute text-amber-200 bottom-full  duration-150 group-hover/button:bottom-0 leading-none">
+              Subscribe
+            </span>
+          </button>
+        </li>
+      </ul>
     </main>
   );
 }
