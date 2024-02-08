b319fe7d9b0f7044831476b5e1d7b6ddb74bf788
brunobrunoo114 <truongtritin.bee@gmail.com>
2024-02-08 17:59:20 +0700

    feat: add menu items functions and animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,67 @@
 "use client";
-import HomepageCta from "@/pages/homepage/cta";
-import HomepageExpertise from "@/pages/homepage/expertise";
-import HompageHero from "@/pages/homepage/hero";
-import HomepageProject from "@/pages/homepage/project";
-import HomepageValue from "@/pages/homepage/value";
+import { header } from "@/data/header.json";
+import gsap from "gsap";
+import { useEffect, useState } from "react";
+
+function calculateWidths(array: typeof header) {
+  const baseWidth = 100;
+  const increment = baseWidth / array.length;
+  return array.map((_, index) => {
+    const width = (index + 1) * increment; // Calculate with index and increment
+    return `${width}%`; // Apply Tailwind CSS class
+  });
+}
+
 export default function Home() {
+  const [testArray, setTestArray] = useState<string[]>();
+
+  useEffect(() => {
+    setTestArray(calculateWidths(header));
+  }, []);
+
+  const closeMenu = () => {
+    const tl = gsap.timeline();
+
+    const context = gsap.context(() => {
+      tl.to(".menu_item", {
+        width: 0,
+        ease: "power4.inOut",
+        stagger: 0.1,
+      });
+    });
+  };
+
+  const openMenu = () => {
+    testArray?.forEach((_, index) => {
+      const menuItem = document.querySelectorAll(".menu_item")[index]; // Select the specific menu item
+      gsap.to(menuItem, {
+        width: testArray[index], // Apply the width from the array
+        ease: "power4.inOut",
+        delay: index * 0.1,
+      });
+    });
+
+    const tl = gsap.timeline();
+    const context = gsap.context(() => {});
+  };
+
   return (
-    <main className="rounded-3xl h-fit overflow-y-scroll  no-scrollbar">
-      <HompageHero />
-      <HomepageCta />
-      <HomepageProject />
-      <HomepageExpertise />
-      <HomepageValue />
+    <main className="h-screen overflow-y-scroll  no-scrollbar">
+      <button onClick={() => closeMenu()}>Close</button>
+      <button onClick={() => openMenu()}>Open</button>
+      <ul className="h-full flex flex-col-reverse justify-between">
+        {header.map((item, index) => {
+          return (
+            <li
+              key={index}
+              style={{ width: `${testArray?.[index]}` }}
+              className={`menu_item text-[3vw] font-bold bg-bruno-black h-full text-bruno-white`}
+            >
+              {item.label}
+            </li>
+          );
+        })}
+      </ul>
     </main>
   );
 }
