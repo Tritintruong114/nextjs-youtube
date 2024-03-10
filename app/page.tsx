f1ad5f6e4c5bf2cc0c42543a7e5aedcf40a7a7e2
brunobrunoo114 <truongtritin.bee@gmail.com>
2024-03-10 23:27:46 +0700

    feat: deleted components " "

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,4 @@
 "use client";
 export default function Home() {
-  return (
-    <main className="no-scrollbar">
-      <div className="h-screen w-screen flex justify-center items-center">
-        <h1 className="bg-white text-6xl font-bebas font-bold">
-          Next JS Starter
-        </h1>
-      </div>
-    </main>
-  );
+  return <main className="no-scrollbar"></main>;
 }
