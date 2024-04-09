262e2eb7dfc38d8bcc4c729962456201986e0046
brunobrunoo114 <truongtritin.bee@gmail.com>
2024-04-09 23:00:51 +0700

    feat: updated card list

--- components/elements/card/card-list.tsx
+++ components/elements/card/card-list.tsx
@@ -2,7 +2,11 @@ import Card from "./card";
 
 const CardsList = () => {
   return (
-    <div className="h-full sm:h-3/4 md:h-1/2 grid gap-3 py-3 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 justify-center items-center">
+    <div className="h-full grid gap-3 py-3 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 justify-center items-center">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
       <Card />
       <Card />
       <Card />
